9a39541b8dfadb37c2fe00f589edead140c153b2
Edwin <76659978+EdwinFranco0903@users.noreply.github.com>
2023-08-15 14:01:52 -0600

    Update CursoPythonCL6.py

--- CursoPythonCL6.py
+++ CursoPythonCL6.py
@@ -5,4 +5,19 @@ if numero < 10:
 print('numero es menor que 10')
 
 numero = 8
-if  numero > 5
+if  numero > 5:
+  print('numero ')
+
+
+#hacer mientras cumpla condicion, indica la condicion de salida al llegar al valor
+
+#variable bandera
+flag = 0
+while flag <5:
+  print(flag)
+  flag = flag + 1
+
+flag = 0
+while flag <5:
+  print(flag)
+  flag += 1
