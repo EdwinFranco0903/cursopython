56a6b71738f222d62e2167871bb904382cf6d216
Edwin <76659978+EdwinFranco0903@users.noreply.github.com>
2023-08-16 16:27:04 -0600

    Update CursoPythonCL6.py

--- CursoPythonCL6.py
+++ CursoPythonCL6.py
@@ -21,3 +21,26 @@ flag = 0
 while flag <5:
   print(flag)
   flag += 1
+
+nombre = input('Ingrese su nombre')
+datoCant = int(('Ingrese cantidad de nombre'))
+
+do print(nombre):
+while(datoCant > 0 )
+
+nombre = input("¿Cómo te llamas? ")
+n = input("Introduce un número entero: ")
+print((nombre + "\n") * int(n))
+
+
+
+numero=int(input("num between 0 and 5, print num times hello world"))
+
+while numero > 5:
+    numero=int(input("num between 0 and 5, print num times hello world"))
+
+i=0
+while i < numero:
+    print ("Hola mundo")
+    i=i+1
+
